09689e76b21c539c744d74558551b18ed915c6f6
Nero Sant <nerosals@hotmail.com>
2022-07-19 13:38:32 -0300

    procfile

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -168,9 +168,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static2")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 
 CRISPY_TEMPLATE_PACK="bootstrap4"
