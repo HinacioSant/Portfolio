de2fe1e021bf3b697a0496d251e0a0c7e7dd492d
Nero Sant <nerosals@hotmail.com>
2022-12-01 11:28:30 -0300

    Small changes

--- Main/views.py
+++ Main/views.py
@@ -5,9 +5,6 @@ def index(request):
     return render(request, "PTs/index.html")
 
 def projects(request):
-    if not request.user.is_authenticated:
-        return redirect("/lg")
-
     return render(request, "PTs/projects.html")
 
 def resume(request):
