1e9085a29b67f94042e50e7b98f86535fba08340
Nero Sant <nerosals@hotmail.com>
2023-05-04 08:32:27 -0300

    update

--- Main/views.py
+++ Main/views.py
@@ -1,17 +1,11 @@
 from django.shortcuts import render, redirect
-from django.views.debug import ExceptionReporter
+
 
 # Create your views here.
 
 
 
 def index(request):
-    print(request.META['HTTP_HOST'])
-    reporter = ExceptionReporter(request, exc_type, exc_value, traceback)
-    html_report = reporter.get_traceback_html()
-    text_report = reporter.get_traceback_text()
-    print(html_report)
-    print(text_report)
     return render(request, "PTs/index.html")
 
 def projects(request):
--- Portfolio/wsgi.py
+++ Portfolio/wsgi.py
@@ -8,15 +8,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-import traceback
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Portfolio.settings')
 
-
-try:
-    # your Django application code here
-    application = get_wsgi_application()
-
-except Exception as e:
-    traceback.print_exc()
+application = get_wsgi_application()
