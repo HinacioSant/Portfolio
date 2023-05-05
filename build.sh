12723d5524fd9bdd0802ea12bcf3407c8519b539
Nero Sant <nerosals@hotmail.com>
2023-05-05 09:17:07 -0300

    update

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# exit on error
+set -o errexit
+
+
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Apply database migrations
+python manage.py migrate
+
+# Create a superuser
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$SUPERUSER_USERNAME', '$SUPERUSER_EMAIL', '$SUPERUSER_PASSWORD')" | python manage.py shell
+
+
+# Install Python dependencies
+pip install --no-cache-dir -r /path/to/requirements.txt
